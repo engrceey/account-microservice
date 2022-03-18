1a68d7c29492764c98dda98b6c5e76b1263c309a
engrceey <ogbondachristian@gmail.com>
2022-03-18 15:43:40 +0100

    transfer dto

---
+++ src/main/java/com/reloadly/accountservice/dto/response/TransferResponseDto.java
@@ -0,0 +1,15 @@
+package com.reloadly.accountservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TransferResponseDto {
+    private String statusCode;
+    private String senderName;
+    private long sendAccountNumber;
+    private String receiverName;
+    private boolean isTransactionSuccessful;
+}
